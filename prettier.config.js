1ee7f4c61b6d0063bdf7e208998c89ea445ae138
yax <yakeen.sewsanker@gmail.com>
2023-09-03 22:14:00 +0100

    chore: project setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  printWidth: 120,
+  proseWrap: 'always',
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  useTabs: false
+};
